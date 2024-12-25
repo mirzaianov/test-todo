f054c08172aeed84ee097baa4dbedb6e6d987f7e
Riaz Mirzaianov <7o.mseJak@gmail.com>
2024-12-25 19:57:03 +0300

    test: Correct info fields

--- src/tests/Status.test.tsx
+++ src/tests/Status.test.tsx
@@ -4,10 +4,9 @@ import { type Todos, type Filter } from '../types/types';
 import userEvent from '@testing-library/user-event';
 import Status from '../components/Status';
 
-describe('Status Component', () => {
+describe('Status', () => {
   const mockSetFilter = vi.fn();
   const mockClearCompleted = vi.fn();
-  const user = userEvent.setup();
 
   const renderComponent = (activeTodos: Todos, filter: Filter) => {
     render(
@@ -21,19 +20,19 @@ describe('Status Component', () => {
   };
 
   //Rendering test - No active todos
-  it('should display "Items completed" when there are no active todos', () => {
+  it('should render `Items completed` when there are no active todos', () => {
     renderComponent([], 'all');
     expect(screen.getByText('Items completed')).toBeInTheDocument();
   });
 
   //Rendering test - One active todo
-  it('should display "1 item left" when there is one active todo', () => {
+  it('should render `1 item left` when there is one active todo', () => {
     renderComponent([{ id: 1, text: 'Test Todo', completed: false }], 'all');
     expect(screen.getByText('1 item left')).toBeInTheDocument();
   });
 
   //Rendering test - Multiple active todos
-  it('should display the correct number of active todos', () => {
+  it('should render the correct number of active todos', () => {
     renderComponent(
       [
         { id: 1, text: 'Test Todo 1', completed: false },
@@ -45,28 +44,36 @@ describe('Status Component', () => {
   });
 
   // Click event test - All button
-  it('should call setFilter with "all" when All button is clicked', async () => {
+  it('should call `setFilter` with `all` when `All` button is clicked', async () => {
+    const user = userEvent.setup();
+
     renderComponent([], 'all');
     await user.click(screen.getByText('All'));
     expect(mockSetFilter).toHaveBeenCalledWith('all');
   });
 
   // Click event test - Active button
-  it('should call setFilter with "active" when Active button is clicked', async () => {
+  it('should call `setFilter` with `active` when `Active` button is clicked', async () => {
+    const user = userEvent.setup();
+
     renderComponent([], 'all');
     await user.click(screen.getByText('Active'));
     expect(mockSetFilter).toHaveBeenCalledWith('active');
   });
 
   // Click event test - Completed button
-  it('should call setFilter with "completed" when Completed button is clicked', async () => {
+  it('should call `setFilter` with `completed` when `Completed` button is clicked', async () => {
+    const user = userEvent.setup();
+
     renderComponent([], 'all');
     await user.click(screen.getByText('Completed'));
     expect(mockSetFilter).toHaveBeenCalledWith('completed');
   });
 
   // Click event test - Clear completed button
-  it('should call clearCompleted when Clear completed button is clicked', async () => {
+  it('should call `clearCompleted` when `Clear completed` button is clicked', async () => {
+    const user = userEvent.setup();
+
     renderComponent([], 'all');
     await user.click(screen.getByText('Clear completed'));
     expect(mockClearCompleted).toHaveBeenCalled();
