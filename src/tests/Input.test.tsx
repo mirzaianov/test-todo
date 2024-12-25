080ce55618bee8f4311b4ae602b04d28af265ba7
Riaz Mirzaianov <7o.mseJak@gmail.com>
2024-12-25 19:57:14 +0300

    test: Add Input test

---
+++ src/tests/Input.test.tsx
@@ -0,0 +1,90 @@
+import { describe, it, expect, vi } from 'vitest';
+import { render, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import Input from '../components/Input';
+
+type InputProps = {
+  isExpanded: boolean;
+  setIsExpanded: (isExpanded: boolean) => void;
+  inputValue: string;
+  setInputValue: (inputValue: string) => void;
+  addTodo: (text: string) => void;
+};
+
+describe('Input', () => {
+  const spySetIsExpanded = vi.fn();
+  const spySetInputValue = vi.fn();
+  const spyAddTodo = vi.fn();
+
+  const defaultProps: InputProps = {
+    isExpanded: true,
+    setIsExpanded: spySetIsExpanded,
+    inputValue: '',
+    setInputValue: spySetInputValue,
+    addTodo: spyAddTodo,
+  };
+
+  // Rendering test
+  it('should render an input with a placeholder and the `Dropdown` button', () => {
+    render(<Input {...defaultProps} />);
+    expect(screen.getByText('❯')).toBeInTheDocument();
+    expect(
+      screen.getByPlaceholderText('What needs to be done?'),
+    ).toBeInTheDocument();
+  });
+
+  // Add button rendering test
+  it('should not render the `Add` button', () => {
+    render(<Input {...defaultProps} />);
+    expect(screen.queryByText('+')).not.toBeInTheDocument();
+  });
+
+  // Input change event test
+  it('should call `setInputValue` on the input change', async () => {
+    const user = userEvent.setup();
+
+    render(<Input {...defaultProps} />);
+
+    const input = screen.getByRole('textbox');
+
+    await user.type(input, 'New Todo');
+    expect(spySetInputValue).toHaveBeenCalledTimes(8);
+  });
+
+  // Click event test - Dropdown button
+  it('should call `setIsExpanded` when the `Dropdown` button is clicked', async () => {
+    const user = userEvent.setup();
+
+    render(<Input {...defaultProps} />);
+    await user.click(screen.getByText('❯'));
+    expect(spySetIsExpanded).toHaveBeenCalledWith(false);
+  });
+
+  // Enter key press event test
+  it('should call `addTodo` on the `Enter key` press', async () => {
+    const user = userEvent.setup();
+
+    render(
+      <Input
+        {...defaultProps}
+        inputValue="New Todo"
+      />,
+    );
+    await user.keyboard('{Enter}');
+    expect(spyAddTodo).toHaveBeenCalledWith('New Todo');
+  });
+
+  // Click event test - Add button
+  it('should call `addTodo` when the `Add` button is clicked', async () => {
+    const user = userEvent.setup();
+
+    render(
+      <Input
+        {...defaultProps}
+        inputValue="New Todo"
+      />,
+    );
+    await user.click(screen.getByText('+'));
+    expect(spyAddTodo).toHaveBeenCalledWith('New Todo');
+  });
+});
