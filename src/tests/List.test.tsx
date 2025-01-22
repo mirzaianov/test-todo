d448b15214faa077a9025fa97670bf10d0be8989
Riaz Mirzaianov <7o.mseJak@gmail.com>
2025-01-22 18:24:11 +0300

    test: Complete List test file

--- src/tests/List.test.tsx
+++ src/tests/List.test.tsx
@@ -18,12 +18,101 @@ describe('List', () => {
   };
 
   // Initial rendering test
-  it('should render the list', () => {
+  it('should be accessible but empty', () => {
     render(<List {...defaultProps} />);
 
     const listElement = screen.getByRole('list');
 
     expect(listElement).toBeInTheDocument();
     expect(listElement.children).toHaveLength(0);
+  });
+
+  // .Todos rendering test
+  it('should render the correct number of passed in todos', () => {
+    render(
+      <List
+        {...defaultProps}
+        filteredTodos={[
+          { id: 1, text: 'Test Todo', completed: false },
+          { id: 2, text: 'Test Todo', completed: true },
+        ]}
+      />,
+    );
+
+    expect(screen.getAllByRole('listitem')).toHaveLength(2);
+  });
+
+  // Toggle event test
+  it('should call `toggleTodo` when a todo is clicked', async () => {
+    const user = userEvent.setup();
+
+    render(
+      <List
+        {...defaultProps}
+        filteredTodos={[
+          { id: 1, text: 'Test Todo 1', completed: false },
+          { id: 2, text: 'Test Todo 2', completed: true },
+        ]}
+      />,
+    );
+    await user.click(screen.getAllByRole('checkbox')[0]);
+    expect(spyToggleTodo).toHaveBeenCalledWith(1);
+  });
+
+  // Checkbox test
+  it('should have a todo with the correct checked state', () => {
+    render(
+      <List
+        {...defaultProps}
+        filteredTodos={[
+          { id: 1, text: 'Test Todo 1', completed: false },
+          { id: 2, text: 'Test Todo 2', completed: true },
+        ]}
+      />,
+    );
+
+    const checkboxes = screen.getAllByRole('checkbox');
+
+    expect(checkboxes[0]).not.toBeChecked();
+    expect(checkboxes[1]).toBeChecked();
+  });
+
+  // Style test
+  it('should have a todo with the correct text style', () => {
+    const { container } = render(
+      <List
+        {...defaultProps}
+        filteredTodos={[
+          { id: 1, text: 'Test Todo 1', completed: false },
+          { id: 2, text: 'Test Todo 2', completed: true },
+        ]}
+      />,
+    );
+
+    const spans = container.querySelectorAll('span');
+
+    expect(spans[0]).not.toHaveClass(styles.textCompleted);
+    expect(spans[1]).toHaveClass(styles.textCompleted);
+  });
+
+  // Expanded class test - Expanded
+  it('should have the `wrapperExpanded` class when `isExpanded` is true', () => {
+    const { container } = render(<List {...defaultProps} />);
+    const firstElement = container.firstElementChild;
+
+    expect(firstElement).toHaveClass(styles.wrapperExpanded);
+  });
+
+  // Expanded class test - Collapsed
+  it('should not have the `wrapperExpanded` class when `isExpanded` is false', () => {
+    const { container } = render(
+      <List
+        {...defaultProps}
+        isExpanded={false}
+      />,
+    );
+    const firstElement = container.firstElementChild;
+
+    expect(firstElement).not.toHaveClass(styles.wrapperExpanded);
   });
 });
